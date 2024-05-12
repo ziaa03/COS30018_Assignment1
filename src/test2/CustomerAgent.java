8e470157f509fb5d44059572b178b6f0c9d10983
DarrenLau <laudarren911@gmail.com>
2024-05-12 23:08:39 +0800

    Update CustomerAgent.java

--- src/test2/CustomerAgent.java
+++ src/test2/CustomerAgent.java
@@ -8,22 +8,42 @@ import jade.lang.acl.ACLMessage;
 import java.awt.Color;
 import java.util.*;
 
-public class CustomerAgent extends Agent {
-    private RouteUI ui;
-    private Random random;
-    private List<ParcelList> parcelLists;  // Define the parcel lists at the class level
-    private int currentListIndex = 0;      // Index to track current parcel list
-    private int currentX, currentY; 
-
-    public CustomerAgent(RouteUI ui) {
-        this.ui = ui;
+public class CustomerAgent extends Agent
+{
+	private RouteUI ui;
+	private Random random;
+	private List<ParcelList> parcelLists;
+	private int currentListIndex = 0;
+	private int currentX, currentY;
+	
+	public CustomerAgent() {
+        super();
+        // Optionally initialize fields with default values
+        this.ui = new RouteUI(); // Assuming default initialization is acceptable
         this.random = new Random();
-        initializeParcelLists(); // Initialize the parcel lists upon agent creation
-        NewLocation();
+        initializeParcelLists();
+        updateLocation();
     }
 
+	public CustomerAgent(RouteUI ui) {
+        this.ui = ui;
+        this.random = new Random();
+        initializeParcelLists();
+        updateLocation();
+    }
+	
+    protected void setup()
+    {
+        System.out.println("\u001B[30m" + "--------- INITIALISATION STATUSES ---------" + "\u001B[0m");
+        System.out.println("Customer agent is ready.");
+
+        initializeParcelLists();
+
+        // Add ticker behavior to periodically send parcel lists
+        addBehaviour(new ParcelTicker(this, parcelLists));
+    }
+    
     private void initializeParcelLists() {
-        // Initialization of parcel lists
         parcelLists = Arrays.asList(
                 new ParcelList("Region A", Arrays.asList(
                         new Parcel("a", 5, 1, 1), new Parcel("b", 10, 3, 1),
@@ -40,61 +60,65 @@ public class CustomerAgent extends Agent {
         );
     }
 
-    protected void setup() {
-        System.out.println("\u001B[30m" + "--------- INITIALISATION STATUSES ---------" + "\u001B[0m");
-        System.out.println("Customer agent is ready.");
-        addBehaviour(new ParcelTicker(this, 5000)); // Add behavior with defined tick time
-    }
-
     private class ParcelTicker extends TickerBehaviour {
-        public ParcelTicker(Agent a, long period) {
-            super(a, period);
+        private List<ParcelList> parcelLists;
+        
+
+        public ParcelTicker(Agent a, List<ParcelList> parcelLists) {
+            super(a, 5000); // Ticker interval: 5000 milliseconds (adjust as needed)
+            this.parcelLists = parcelLists;
         }
 
         protected void onTick() {
             if (currentListIndex < parcelLists.size()) {
                 sendParcels(parcelLists.get(currentListIndex));
-                NewLocation();  // Update location based on the current parcel list
                 currentListIndex++;
             } else {
-                stop();  // Stop the ticker behavior once all lists are processed
+                // Stop the ticker behavior once all parcel lists are sent
+                stop();
             }
         }
     }
 
     private void sendParcels(ParcelList parcelList) {
-        StringBuilder messageContent = new StringBuilder("Parcel Region: ").append(parcelList.name).append("\n");
-        int totalWeight = 0;
+        // Construct message content with information of parcels in the list
+        StringBuilder messageContent = new StringBuilder();
+        // Append the name of the parcel region
+        messageContent.append("Parcel Region: ").append(parcelList.name).append("\n");
+        int totalWeight = 0; // Initialize total weight for the current parcel list
         for (Parcel parcel : parcelList.parcels) {
-            messageContent.append("Parcel: ").append(parcel.name).append(", Weight: ")
-                          .append(parcel.weight).append(", Location: (")
-                          .append(parcel.x).append(", ").append(parcel.y).append(")\n");
-            totalWeight += parcel.weight;
+            messageContent.append("Parcel: ").append(parcel.name).append(", Weight: ").append(parcel.weight)
+                    .append(", Location: (").append(parcel.x).append(", ").append(parcel.y).append(")\n");
+            totalWeight += parcel.weight; // Accumulate the weight of each parcel
         }
+        // Print out the total weight for the current parcel list
         Utilities.printMessageWithoutAgent("\u001B[30m" + "\n----- Total weight of all parcels in " + parcelList.name + ": " + totalWeight + " ----- " + "\u001B[0m");
+
+        // Create ACL message and send to MasterRoutingAgent
         ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
         msg.addReceiver(new AID("MasterRoutingAgent", AID.ISLOCALNAME));
         msg.setContent(messageContent.toString());
         send(msg);
     }
-
-    public void NewLocation() {
-        for (ParcelList currentParcelList : parcelLists) {  // Iterate over all parcel lists
-            for (Parcel parcel : currentParcelList.getParcels()) {  // Iterate over all parcels in the current list
-                int x = parcel.getX();  // Assuming getter method getX() is used
-                int y = parcel.getY();  // Assuming getter method getY() is used
-                ui.updateGrid(x, y, Color.BLUE);  // Update grid at each parcel's location
-                currentX = x;  // Update currentX to the last processed parcel's X
-                currentY = y;  // Update currentY to the last processed parcel's Y
+    
+    public void updateLocation() {
+        if (!parcelLists.isEmpty()) {
+            for (ParcelList currentParcelList : parcelLists) {
+                for (Parcel parcel : currentParcelList.getParcels()) {
+                    int x = parcel.getX();
+                    int y = parcel.getY();
+                    if (x != currentX || y != currentY) {
+                        ui.updateGrid(x, y, Color.BLUE);
+                        currentX = x;
+                        currentY = y;
+                    }
+                }
             }
         }
     }
 
-    
     public List<ParcelList> getParcelLists() {
-        return this.parcelLists;  // Return a direct reference to the parcelLists
+        return this.parcelLists;
     }
-    
-
     
 }
